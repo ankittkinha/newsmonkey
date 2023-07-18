25dbe972914fdb1c4116f2f5ad84c728609857d2
ankittkinha <ankittkinha@gmail.com>
2023-07-18 19:53:50 +0530

    Fixed some css

--- src/components/NewsItem/index.jsx
+++ src/components/NewsItem/index.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import defaultImg from "../../images/Image_not_available.png";
 
 const NewsItem = (props) => {
 
         const { title, description, imageUrl, newsUrl, author, date, source } = props;
+
+        const replaceImg = (error) => {
+            error.target.src = defaultImg
+        }
+
+
         return (
             <div className="my-3">
-                <div className="card">
+                <div className="card news-card">
                     <div
                         style={{
                             display: "flex",
@@ -22,12 +29,13 @@ const NewsItem = (props) => {
                         </span>
                     </div>
 
-                    <img src={imageUrl} className="card-img-top" alt="Img" />
+
+                    <img src={imageUrl} height={"226px"} className="card-img-top" alt={"image"} onError={replaceImg}/>
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
+                        <h5 className="card-title news-title">{title}</h5>
+                        <p className="card-text news-desc">{description ? description : "The description for this news is not available due to some technical reasons. We'll try to fix this as soon as possible. Sorry for the inconvenience."}</p>
                         <p className="card-text">
-                            <small className="text-body-secondary">
+                            <small className="text-body-secondary news-author">
                                 By {author ? author : "Unknown"} on{" "}
                                 {new Date(date).toGMTString()}
                             </small>
